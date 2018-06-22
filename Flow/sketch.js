e69a230cc2de9a3f4ae1b5669eae0205076d09bc
dinda-dinda <dindadindaart@gmail.com>
2018-06-22 17:45:56 -0300

    airEnergy.js

--- Flow/sketch.js
+++ Flow/sketch.js
@@ -8,61 +8,66 @@ var debug = true;
 
 // Flowfield object
 var flowfield;
-// An ArrayList of vehicles
+// Arrays of vehicles
 var vehicles = [];
+var airEnergys = [];
+
 var sliderSeparateForce;
 var sliderSeekForce;
 
 
 function setup() {
 
-  createCanvas(windowWidth, windowHeight);
-  // Make a new flow field with "resolution" of 16
-  
-  sliderSeparateForce = createSlider(0,1000,50,1);
-  sliderSeekForce = createSlider(0,1000,50,1);
-  
-  flowfield = new FlowField(16);
-  // Make a whole bunch of vehicles with random maxspeed and maxforce values
-  for (var i = 0; i < 120; i++) {
-    vehicles.push(new Vehicle(random(width/3), random(height), random(2, 5), random(0.1, 0.5)));
-  }
+    createCanvas(windowWidth, windowHeight);
+    // Make a new flow field with "resolution" of 16
+
+    sliderSeparateForce = createSlider(0, 1000, 50, 1);
+    sliderSeekForce = createSlider(0, 1000, 50, 1);
+
+    flowfield = new FlowField(16);
+    // Make a whole bunch of vehicles with random maxspeed and maxforce values
+    for (var i = 0; i < 120; i++) {
+        vehicles.push(new Vehicle(random(width / 3), random(height), random(2, 5), random(0.1, 0.5)));
+    }
 }
 
 function draw() {
-  background(10,10);
-  // Display the flowfield in "debug" mode
-  if (debug) flowfield.display();
-  // Tell all the vehicles to follow the flow field
-  for (var i = 0; i < vehicles.length; i++) {
-    /*si los vehicles estan mas alla del tercio de la pantalla*/
-    if(vehicles[i].position.x > windowWidth/3){ 
-      
-    /*aplico seek and separate force and follow no*/
-    
-      vehicles[i].applyBehaviors(vehicles);
-   
-     // vehicles[i].follow(flowfield);
-    } else {
-      
-    /*si no que sigan el followfield*/
-    vehicles[i].follow(flowfield);
-    }
-    vehicles[i].run();
-  }
+    let target = createVector(mouseX, mouseY);
+    background(220);
+    // Display the flowfield in "debug" mode
+    if (debug) flowfield.display();
+    // Tell all the vehicles to follow the flow field
+    for (var i = 0; i < vehicles.length; i++) {
+        /*si los vehicles estan mas alla del tercio de la pantalla*/
+        if (vehicles[i].position.x > windowWidth / 3) {
+            let coordX = int(vehicles[i].position.x);
+            let coordY = int(vehicles[i].position.y);
 
+            airEnergys.push(new AirEnergy(coordX, coordY));
+        } else {
+
+            vehicles[i].follow(flowfield); /*si no que sigan el followfield*/
+            vehicles[i].run();
+        }
+        for (var j = 0; j < airEnergys.length; j++) {
+            airEnergys[j].applyBehaviorsAir(airEnergys);
+            airEnergys[j].updateAir();
+            airEnergys[j].bordersAir();
+            airEnergys[j].displayAir();
+
+        }
+
+    }
 }
 
 
 function keyPressed() {
-  if (key == ' ') {
-    debug = !debug;
-  }
+    if (key == ' ') {
+        debug = !debug;
+    }
 }
 
 // Make a new flowfield
 function mousePressed() {
-  flowfield.init();
+    flowfield.init();
 }
-
-
--- Flow/vehicle.js
+++ Flow/vehicle.js
@@ -4,132 +4,158 @@
 
 // The "Vehicle" constructor
 
-function Vehicle(x,y,ms,mf) {
-  this.position = createVector(x,y);
-  this.acceleration = createVector(0,0);
-  this.velocity = createVector(0,0);
-  this.r = 4;
-  this.maxspeed = ms || 4;
-  this.maxforce = mf || 0.1;
+function Vehicle(x, y, ms, mf) {
+    this.position = createVector(x, y);
+    this.velocity = createVector(0, 0);
+    this.acceleration = createVector(0, 0);
+    this.r = 4;
+    this.maxspeed = ms || 5;
+    this.maxforce = mf || 0.2;
 
-  this.run = function() {
-    this.update();
-    this.borders();
-    this.display();
-  }
 
-  // Implementing Reynolds' flow field following algorithm
-  // http://wwwred3d.com/cwr/steer/FlowFollow.html
-  this.follow = function(flow) {
-    
-    var desired = flow.lookup(this.position);  // What is the vector at that spot in the flow field?
-    desired.mult(this.maxspeed);               // Scale it up by maxspeed
-    
-    var steer = p5.Vector.sub(desired, this.velocity);    // Steering is desired minus velocity
-    steer.limit(this.maxforce);                         // Limit to maximum steering force
-    this.applyForce(steer);
-  }
-
-  this.applyForce = function(force) {
-    // We could add mass here if we want A = F / M
-
-    //Agregar!!!!!!!!!!!!!
-    this.acceleration.add(force);
-  }
-
-  this.applyBehaviors = function(vehicles){
-    
-    var separateForce = this.separate(vehicles);
-    var seekForce = this.seek(createVector(mouseX,mouseY));
-    
-    separateForce.mult(sliderSeparateForce.value());
-    seekForce.mult(sliderSeekForce.value());
-    
-    this.applyForce(separateForce);
-    this.applyForce(seekForce);
-  }
-  
-  /*------------seek-----------------*/
-   // A method that calculates a steering force towards a target
-  // STEER = DESIRED MINUS VELOCITY
-  
-  
-  this.seek = function(target){
-    var desired = p5.Vector.sub(target,this.pos);
-    desired.normalize();                            //normalizo y lo paso a su maxima velocidad
-    desired.mult(this.maxspeed);
-    var steer = p5.Vector.sub(desired,this.velocity); //steer minus desired velocity
-    steer.limit(this.maxforce);                       // Limit to maximum steering force
-    return steer;
-  }
-  
-  
-  /*------------separate-----------------*/
-  this.separate = function(vehicles){
-    var desiredseparation = 20;
-    var sum = createVector();
-    var count = 0;
-    //check if its to close
-     for (var i = 0; i < vehicles.length; i++) {
-      var d = p5.Vector.dist(this.position, vehicles[i].position);
-      // If the distance is greater than 0 and less than an arbitrary amount (0 when you are yourself)
-    
-      if ((d > 0) && (d < desiredseparation)) {
-        
-        // Calculate vector pointing away from neighbor
-        var diff = p5.Vector.sub(this.position, vehicles[i].position);
-        diff.normalize();
-        diff.div(d);        // Weight by distance
-        sum.add(diff);
-        count++;            // Keep track of how many
-      }
+    this.run = function() {
+        this.update();
+        this.borders();
+        this.display();
     }
-  // Average -- divide by how many
-    if (count > 0) {
-      sum.div(count);
-      // Our desired vector is the average scaled to maximum speed
-      sum.normalize();
-      sum.mult(this.maxspeed);
-      // Implement Reynolds: Steering = Desired - Velocity
-      sum.sub(this.velocity);
-      sum.limit(this.maxforce);
+
+    this.runAir = function() {
+        this.update();
+        this.borders();
+        this.displayAir();
     }
-    return sum;
-  }
-/**----------------------------------------------*/
-  
+
+    // Implementing Reynolds' flow field following algorithm
+    // http://wwwred3d.com/cwr/steer/FlowFollow.html
+    this.follow = function(flow) {
+        if (this.position.x < width / 3) {
+            var desired = flow.lookup(this.position); // What is the vector at that spot in the flow field?
+            desired.mult(this.maxspeed); // Scale it up by maxspeed
+
+
+            var steer = p5.Vector.sub(desired, this.velocity); // Steering is desired minus velocity
+            steer.limit(this.maxforce); // Limit to maximum steering force
+            this.applyForce(steer);
+        }
+    }
+
+    this.applyForce = function(force) {
+        this.acceleration.add(force);
+        //   add mass here  A = F / M
+
+    }
+
+    this.applyBehaviors = function(vehicles) {
+
+        var separateForce = this.separate(vehicles);
+        var seekForce = this.seek(createVector(mouseX, mouseY));
+
+        separateForce.mult(sliderSeparateForce.value());
+        seekForce.mult(sliderSeekForce.value());
+
+        this.applyForce(separateForce);
+        this.applyForce(seekForce);
+    }
+
+    /*------------seek-----------------*/
+    // A method that calculates a steering force towards a target
+    // STEER = DESIRED MINUS VELOCITY
+
+
+    this.seek = function(target) {
+        var target = createVector(mouseX, mouseY);
+
+        var desired = p5.Vector.sub(target, this.pos);
+        desired.normalize(); //normalizo y lo paso a su maxima velocidad
+        desired.mult(this.maxspeed);
+        var steer = p5.Vector.sub(desired, this.velocity); //steer minus desired velocity
+        steer.limit(this.maxforce); // Limit to maximum steering force
+        return steer;
+    }
+
+
+    /*------------separate-----------------*/
+    this.separate = function(vehicles) {
+        var desiredseparation = 20;
+        var sum = createVector();
+        var count = 0;
+        //check if its to close
+        for (var i = 0; i < vehicles.length; i++) {
+            var d = p5.Vector.dist(this.position, vehicles[i].position);
+            // If the distance is greater than 0 and less than an arbitrary amount (0 when you are yourself)
+
+            if ((d > 0) && (d < desiredseparation)) {
+
+                // Calculate vector pointing away from neighbor
+                var diff = p5.Vector.sub(this.position, vehicles[i].position);
+                diff.normalize();
+                diff.div(d); // Weight by distance
+                sum.add(diff);
+                count++; // Keep track of how many
+            }
+        }
+        // Average -- divide by how many
+        if (count > 0) {
+            sum.div(count);
+            // Our desired vector is the average scaled to maximum speed
+            sum.normalize();
+            sum.mult(this.maxspeed);
+            // Implement Reynolds: Steering = Desired - Velocity
+            sum.sub(this.velocity);
+            sum.limit(this.maxforce);
+        }
+        return sum;
+    }
+    /**----------------------------------------------*/
+
     // Method to update location
-  this.update = function() {
-    // Update velocity
-    this.velocity.add(this.acceleration);
-    // Limit speed
-    this.velocity.limit(this.maxspeed);
-    this.position.add(this.velocity);
-    // Reset accelerationelertion to 0 each cycle
-    this.acceleration.mult(0);
-  }
+    this.update = function() {
+        
+        this.velocity.add(this.acceleration); /* Update velocity*/
+        this.velocity.limit(this.maxspeed); /*Limit speed*/
+        this.position.add(this.velocity);
+        
+        this.acceleration.mult(0); /*Reset accelerationelertion to 0 each cycle*/
+    }
 
-  // Wraparound
-  this.borders = function() {
-    if (this.position.x < -this.r) this.position.x = width+this.r;
-    if (this.position.y < -this.r) this.position.y = height+this.r;
-    if (this.position.x > width+this.r) this.position.x = -this.r;
-    if (this.position.y > height+this.r) this.position.y = -this.r;
-  }
+    // Wraparound
+    this.borders = function() {
+        if (this.position.x < -this.r) this.position.x = width + this.r;
+        if (this.position.y < -this.r) this.position.y = height + this.r;
+        if (this.position.x > width + this.r) this.position.x = -this.r;
+        if (this.position.y > height + this.r) this.position.y = -this.r;
+    }
 
-  this.display = function() {
-    // Draw a triangle rotated in the direction of velocity
-    var theta = this.velocity.heading() + PI/2;
-    fill(250,0,random(180,220));
-    noStroke(200);
-    push();
-    translate(this.position.x,this.position.y);
-    rotate(theta);
-    beginShape();
-    vertex(0, -this.r*2);
-    vertex(-this.r, this.r*2);
-    vertex(this.r, this.r*2);
-    endShape(CLOSE);
-    pop();
-  }
+    this.displayAir = function() {
+        // Draw a triangle rotated in the direction of velocity
+
+        var theta = this.velocity;
+        fill(100, random(180, 220),0 );
+        noStroke(200);
+        push();
+        translate(this.position.x, this.position.y);
+        rotate(theta);
+        beginShape();
+        vertex(0, -this.r * 2);
+        vertex(-this.r, this.r * 2);
+        vertex(this.r, this.r * 2);
+        endShape(CLOSE);
+        pop();
+    }
+
+    this.display = function() {
+        // Draw a triangle rotated in the direction of velocity
+        var theta = this.velocity.heading() + PI / 2;
+        fill(250, 0, random(180, 220));
+        noStroke(200);
+        push();
+        translate(this.position.x, this.position.y);
+        rotate(theta);
+        beginShape();
+        vertex(0, -this.r * 2);
+        vertex(-this.r, this.r * 2);
+        vertex(this.r, this.r * 2);
+        endShape(CLOSE);
+        pop();
+    }
 }
